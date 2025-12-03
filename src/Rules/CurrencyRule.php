17c0ef439a41488246de0a4f66ab298a3d42325f
Hammed Oyedele <itz.harmid@gmail.com>
2025-12-03 12:11:12 +0100

    feat: add currency rule and complete the money rule validation

---
+++ src/Rules/CurrencyRule.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Devhammed\LaravelBrickMoney\Rules;
+
+use Closure;
+use Devhammed\LaravelBrickMoney\Currency;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Translation\PotentiallyTranslatedString;
+
+class CurrencyRule implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  Closure(string): PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        if (! (is_string($value) && array_key_exists(mb_strtoupper($value), Currency::getCurrencies()))) {
+            $fail(__('brick-money::validation.invalid_currency'));
+        }
+    }
+}
--- src/Rules/MoneyRule.php
+++ src/Rules/MoneyRule.php
@@ -4,17 +4,18 @@ declare(strict_types=1);
 
 namespace Devhammed\LaravelBrickMoney\Rules;
 
-use Closure;
 use Brick\Math\BigNumber;
+use Closure;
 use Devhammed\LaravelBrickMoney\Money;
 use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Translation\PotentiallyTranslatedString;
 
 class MoneyRule implements ValidationRule
 {
     public function __construct(
-        public bool $nullable = true,
         public Money|BigNumber|int|float|string|null $min = null,
         public Money|BigNumber|int|float|string|null $max = null,
+        public ?string $currency = null,
     ) {
         //
     }
@@ -22,10 +23,27 @@ class MoneyRule implements ValidationRule
     /**
      * Run the validation rule.
      *
-     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     * @param  Closure(string): PotentiallyTranslatedString  $fail
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        //
+        if (! is_string($value) && ! is_int($value) && ! is_float($value)) {
+            $fail(__('brick-money::validation.invalid_money'));
+
+            return;
+        }
+
+        /** @var string $currency */
+        $currency = $this->currency ?? config('brick-money.currency');
+
+        $money = Money::of($value, $currency);
+
+        if ($this->min !== null && $money->isLessThan($this->min)) {
+            $fail(__('brick-money::validation.min_money'));
+        }
+
+        if ($this->max !== null && $money->isGreaterThan($this->max)) {
+            $fail(__('brick-money::validation.max_money'));
+        }
     }
 }
