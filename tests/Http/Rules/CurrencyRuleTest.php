ff0854093bc8c12032b451568672e0b213e5eabd
Hammed Oyedele <itz.harmid@gmail.com>
2025-12-03 15:52:24 +0100

    test: add tests for currency rule

--- tests/Http/Rules/CurrencyRuleTest.php
+++ tests/Http/Rules/CurrencyRuleTest.php
@@ -5,8 +5,22 @@ declare(strict_types=1);
 use Devhammed\LaravelBrickMoney\Tests\Http\Requests\CurrencyFormRequest;
 use Illuminate\Support\Facades\Route;
 
+use function Pest\Laravel\postJson;
+
 beforeEach(function () {
     Route::post('/test-currency-rule', function (CurrencyFormRequest $request) {
         return $request->currency('currency');
     });
+});
+
+it('passes validation', function () {
+    postJson('/test-currency-rule', ['currency' => 'USD'])
+        ->assertOk();
+});
+
+it('rejects invalid', function () {
+    postJson('/test-currency-rule', ['currency' => 'NOT_VALID'])
+        ->assertInvalid([
+            'currency' => 'The selected currency is invalid.',
+        ]);
 });
